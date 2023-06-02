c17e1675f3e20b6af72628f390dd6d7e7c27e8a4
Brendan Kersten <1192333@student.roc-nijmegen.nl>
2023-06-02 22:45:26 +0200

    partials map added

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Register.scss';
 export default function Register(){
     return (
-        <div className="register">
-            <h1>Some title here</h1>
-        </div>
+        <body>
+            <header>
+                test ...
+            </header>
+        </body>
     )
 }
