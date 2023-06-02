5f902e2d68b469c37bf449b660cbb3963cf60579
Brendan Kersten <1192333@student.roc-nijmegen.nl>
2023-06-03 00:36:55 +0200

    header of startscreen done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Register from './components/Auth/Register/Register';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Friendify">
      <Register/>
     </div>
   );
--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import darkLogo from '../../../assets/dark-friendify-logo.svg';
+
 import './Register.scss';
 export default function Register(){
     return (
         <body>
-            <header>
-                test ...
+            <header class="startscreen">
+                <img src={darkLogo} alt="Dark Friendify Logo" />
+                <h1>Friendify</h1>
+                <div class="startscreen__line"></div>
+                <h3>Sign up</h3>
             </header>
         </body>
     )
