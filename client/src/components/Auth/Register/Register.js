97493795172f5a078f385f7670808f1ef5d265dd
Erik Todorov <eriktdrv@gmail.com>
2023-06-02 16:23:49 +0300

    Adding register component

---
+++ client/src/components/Auth/Register/Register.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Register.scss';
+export default function Register(){
+    return (
+        <div className="register">
+            <h1>Some title here</h1>
+        </div>
+    )
+}
