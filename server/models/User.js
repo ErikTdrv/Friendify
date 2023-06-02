28b74942cc64696004538c8963558f86d08fe774
Erik Todorov <eriktdrv@gmail.com>
2023-06-02 17:22:29 +0300

    Setting User model

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-
+    firstName: {
+        required: true,
+        minlength: [2, 'Name must have at least 2 characters!'],
+        maxlength: [10, 'Name must not have more than 10 characters!']
+    },
+    lastName: {
+        required: true,
+        minlength: [2, 'Last Name must have at least 2 characters!'],
+        maxlength: [10, 'Last Name must not have more than 10 characters!']
+    }, 
+    email: {
+        required: true,
+        minlength: [8, 'Email must have at least 8 characters!'],
+        maxlength: [30, 'Email must not have more than 30 characters!'],
+    },
+    nationality: {
+        required: true,
+        maxlength: [12, 'Nationality must not have more than 12 characters!'],
+    },
+    password: {
+        required: true,
+        minlength: [6, 'Password must have at least 6 characters!'],
+        maxlength: [15, 'Password must not have more than 15 characters!'],
+    }
 });
 
 userSchema.pre("save", function (next) {
