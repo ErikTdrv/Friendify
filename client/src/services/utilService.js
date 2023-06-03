080741d74acd2f61fd6bf13a997e8a86f86495a3
Erik Todorov <eriktdrv@gmail.com>
2023-06-03 18:15:49 +0300

    Adding utilService

---
+++ client/src/services/utilService.js
@@ -0,0 +1,13 @@
+export const convertToBase64 = async (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+  
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
