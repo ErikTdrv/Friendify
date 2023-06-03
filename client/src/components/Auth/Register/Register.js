3233507ef5f0169f8ba07ad06628e42ed0b481b5
Erik Todorov <eriktdrv@gmail.com>
2023-06-03 17:51:56 +0300

    Adding second page

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -6,86 +6,100 @@ import './Register.scss';
 
 export default function Register() {
   const [isLogin, setIsLogin] = useState(true);
-
+  const [isNextClicked, setIsNextClicked] = useState(false);
   return (
     <section className="startscreen">
       <header className="startscreen">
         <img src={darkLogo} alt="Dark Friendify Logo" />
         <h1>Friendify</h1>
         <div className="startscreen__line"></div>
-        <h3>Sign up</h3> 
+        <h3>Sign up</h3>
       </header>
       <div className='content'>
         <div className='pyramid-container'>
-            <img src={DarkPyramid} alt="Dark Friendify Logo" />
+          <img src={DarkPyramid} alt="Dark Friendify Logo" />
         </div>
         <div className='forms-container'>
-        {isLogin !== undefined ? (
+          {isLogin !== undefined ? (
             <form className={`form-transition ${isLogin ? 'form-HIDDEN' : 'form-VISIBLE'}`}>
-                <div className='first'>
-                    <div className='top-container'>
-                        <div className='input-container first-name'>
-                            <label>First name <span>*</span></label>
-                            <input 
-                                type="text" 
-                                placeholder='Fill in ur first name ...'
-                            />
-                        </div>
-                        <div className='input-container last-name'>
-                            <label>Last name <span>*</span></label>
-                            <input 
-                                type="text" 
-                                placeholder='Fill in ur last name ...'
-                            />
-                        </div>
-                        <div className='input-container email'>
-                            <label>Email name <span>*</span></label>
-                            <input 
-                                type="text" 
-                                placeholder='Fill in ur email ...'
-                            />
-                        </div>
-                        <div className='input-container nationality'>
-                            <label>Nationality <span>*</span></label>
-                            <input 
-                                type="text" 
-                                placeholder='Fill in ur nationality ...'
-                            />
-                        </div>
-                        <div className='input-container first-name'>
-                            <label>Password <span>*</span></label>
-                            <input 
-                                type="password" 
-                                placeholder='Fill in ur password ...'
-                            />
-                        </div>
-                        <div className='input-container first-name'>
-                            <label>Repeat password <span>*</span></label>
-                            <input 
-                                type="password" 
-                                className="first-name"
-                                placeholder='repeat ur password ...'
-                            />
-                        </div>
+              <div className='first'>
+                {!isNextClicked && (
+                  <div className='top-container'>
+                    <div className='input-container first-name'>
+                      <label>First name <span>*</span></label>
+                      <input
+                        type="text"
+                        placeholder='Fill in ur first name ...'
+                      />
                     </div>
-                    <div className='bottom-container'>
-                        <a onClick={() => setIsLogin(false)}>Don't you have an account? Register here</a>
-                        <input
-                            type='submit'
-                            value='Next'
-                        />
+                    <div className='input-container last-name'>
+                      <label>Last name <span>*</span></label>
+                      <input
+                        type="text"
+                        placeholder='Fill in ur last name ...'
+                      />
                     </div>
+                    <div className='input-container email'>
+                      <label>Email name <span>*</span></label>
+                      <input
+                        type="text"
+                        placeholder='Fill in ur email ...'
+                      />
+                    </div>
+                    <div className='input-container nationality'>
+                      <label>Nationality <span>*</span></label>
+                      <input
+                        type="text"
+                        placeholder='Fill in ur nationality ...'
+                      />
+                    </div>
+                    <div className='input-container first-name'>
+                      <label>Password <span>*</span></label>
+                      <input
+                        type="password"
+                        placeholder='Fill in ur password ...'
+                      />
+                    </div>
+                    <div className='input-container first-name'>
+                      <label>Repeat password <span>*</span></label>
+                      <input
+                        type="password"
+                        className="first-name"
+                        placeholder='repeat ur password ...'
+                      />
+                    </div>
+                  </div>
+                )}
+                <div className='bottom-container'>
+                  <a onClick={() => setIsLogin(false)}>Don't you have an account? Register here</a>
+                  {isNextClicked ? (
+                    <input
+                      type='button'
+                      value='Previous'
+                      onClick={() => setIsNextClicked(false)}
+                    />) : (
+                    <input
+                      type='button'
+                      value='Next'
+                      onClick={() => setIsNextClicked(true)}
+                    />)
+                  }
                 </div>
-                <div className='second'></div>
+              </div>
+              {isNextClicked && (
+                <div className='second'>
+                  <label htmlFor="profilepicture">Add Profile Picture<input type='file' id='profilepicture'></input></label>
+                </div>
+              )}
             </form>
-            ) : null }
-            {isLogin !== undefined ? (
-                <form className={`form-transition ${isLogin ? 'form-VISIBLE' : 'form-HIDDEN'}`}>
-                    <div className='bottom-container'>
-                        <a onClick={() => setIsLogin(true)}>Do you already have an account? Login in here</a>
-                    </div>
-                </form>
-            ) : null }
+          ) : null}
+          {isLogin !== undefined ? (
+            <form className={`form-transition ${isLogin ? 'form-VISIBLE' : 'form-HIDDEN'}`}>
+              <div className='bottom-container'>
+                <a onClick={() => setIsLogin(true)}>Do you already have an account? Login in here</a>
+              </div>
+            </form>
+          ) : null}
         </div>
       </div>
     </section>
