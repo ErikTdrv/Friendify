931280c9e6c5423981e95a8ae838fa35ba7fd6e3
Erik Todorov <eriktdrv@gmail.com>
2023-06-03 11:38:30 +0300

    Adding forms

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import darkLogo from '../../../assets/dark-friendify-logo.svg';
 
 import './Register.scss';
 export default function Register(){
+    const [isLogin, setIsLogin] = useState(true);
+    const [isNextClicked, setIsNextClickes] = useState()
     return (
-        <section class="startscreen">
-            <header class="startscreen">
+        <section className="startscreen">
+            <header className="startscreen">
                 <img src={darkLogo} alt="Dark Friendify Logo" />
                 <h1>Friendify</h1>
-                <div class="startscreen__line"></div>
+                <div className="startscreen__line"></div>
                 <h3>Sign up</h3>
             </header>
+            <div></div>
+            {isLogin ? (
+                <form className='register'>
+                    <div onClick={() => setIsLogin(false)}>Don't you have an account? Register here~</div>
+                </form>
+            ) : (
+                <form className='register'>
+
+                    <div onClick={() => setIsLogin(true)}>Do you already have an account? Login in here~</div>
+                </form>
+            )}
+            
         </section>
     )
 }
