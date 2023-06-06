1db55cf52b1a92e6c33090cb7ce51cfcf54b1f12
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 05:40:16 +0300

    Adding register request

---
+++ client/src/services/authService.js
@@ -0,0 +1,21 @@
+const API_URL = 'http://localhost:8080'
+export const register = async (userData) => {
+    try {
+        const response = await fetch(`${API_URL}/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: 'include',
+            body: JSON.stringify(userData),
+        });
+        const data = await response.json();
+        if(response.ok){
+            return data
+        }else {
+            throw new Error(data.error);
+        }
+    } catch (error) {
+        return error;
+    }
+}
