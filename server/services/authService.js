c5a15b40024dbb3fc33adf2006ef6733e9b0bfa7
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 12:39:58 +0300

    Adding login service

--- server/services/authService.js
+++ server/services/authService.js
@@ -27,21 +27,36 @@ const createAccessToken = (user) => {
     };
 }
 
-const register = async(data) => {
-    const existingEmail = await User.findOne({email: data.email})
-    const existingUsername = await User.findOne({username: data.username})
+const register = async (data) => {
+    const existingEmail = await User.findOne({ email: data.email })
+    const existingUsername = await User.findOne({ username: data.username })
 
-    if(existingEmail){
+    if (existingEmail) {
         throw new Error('Email already exists!')
-    }else if(existingUsername){
+    } else if (existingUsername) {
         throw new Error('Username already exists!')
     }
     const user = await User.create(data)
     return createAccessToken(user)
 }
-
+const login = async ({email, password}) => {
+    const user = await User.findOne({email});
+    if(!user){
+        throw new Error('Invalid email or password!')
+    }
+    const isUser = await bcrypt.compare(password, user.password)
+    if(isUser){
+        let userToReturn = createAccessToken(user)
+        userToReturn.avatarImg = user.avatarImg;
+        userToReturn.imageId = user.imageId;
+        return userToReturn
+    }else {
+        throw new Error('Invalid email or password!')
+    }
+}
 module.exports = {
     register,
     validateToken,
     createAccessToken,
+    login
 }
