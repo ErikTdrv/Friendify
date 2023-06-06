6b7ed028158ce7870c21b23aec66d3ef909f437f
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 05:13:38 +0300

    Fixing bug

--- server/models/User.js
+++ server/models/User.js
@@ -2,34 +2,41 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    firstName: {
+    fullName: {
+        type: String,
         required: true,
         minlength: [2, 'Name must have at least 2 characters!'],
         maxlength: [10, 'Name must not have more than 10 characters!']
     },
-    lastName: {
+    nickname: {
+        type: String,
         required: true,
         minlength: [2, 'Last Name must have at least 2 characters!'],
         maxlength: [10, 'Last Name must not have more than 10 characters!']
     }, 
     email: {
+        type: String,
         required: true,
         minlength: [8, 'Email must have at least 8 characters!'],
         maxlength: [30, 'Email must not have more than 30 characters!'],
     },
     nationality: {
+        type: String,
         required: true,
         maxlength: [12, 'Nationality must not have more than 12 characters!'],
     },
     password: {
+        type: String,
         required: true,
         minlength: [6, 'Password must have at least 6 characters!'],
         maxlength: [15, 'Password must not have more than 15 characters!'],
     },
-    avatarImg: {
+    profilePicture: {
+        type: String,
         required: true
     }, 
     imageId: {
+        type: String,
         required: true
     }
 });
