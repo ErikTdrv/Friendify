828897d819dd18ccc46f30527bbc3d1f2b868866
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 12:31:33 +0300

    Adding login request

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -19,3 +19,23 @@ export const register = async (userData) => {
         return error;
     }
 }
+export const login = async (userData) => {
+    try {
+        const response = await fetch(`${API_URL}/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: 'include',
+            body: JSON.stringify(userData),
+        });
+        const data = await response.json();
+        if(response.ok){
+            return data
+        }else {
+            throw new Error(data.error);
+        }
+    } catch (error) {
+        return error;
+    }
+}
