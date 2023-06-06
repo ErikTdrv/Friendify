fd3358b0b2c78a02c2fa4c5c359bb1babd6211e2
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 05:07:46 +0300

    Handling register request

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
-    
-    router.post('/register', (req, res) => {
-        
-    })
-    
+const cloudinary = require('cloudinary');
+const { register } = require('../services/authService');
+
+router.post('/register', async (req, res) => {
+    const data = req.body;
+    const { profilePicture } = req.body;
+    try {
+        if (profilePicture) {
+            const upload = await cloudinary.v2.uploader.upload(profilePicture, {
+                fetch_format: "auto",
+                folder: "Friendify",
+            });
+            data.profilePicture = upload.url;
+            data.imageId = upload.public_id;
+        }
+        const user = await register(data);
+        res.cookie("auth", user.cookie, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+        });
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message });
+    }
+})
+
 module.exports = router;
