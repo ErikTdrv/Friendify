4bd0c92caeccf94e26436963094887e10075c7da
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 05:40:59 +0300

    Configuration of cloudinary uploader

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,6 +5,7 @@ const { register } = require('../services/authService');
 router.post('/register', async (req, res) => {
     const data = req.body;
     const { profilePicture } = req.body;
+    let profilePhotoId;
     try {
         if (profilePicture) {
             const upload = await cloudinary.v2.uploader.upload(profilePicture, {
@@ -13,6 +14,7 @@ router.post('/register', async (req, res) => {
             });
             data.profilePicture = upload.url;
             data.imageId = upload.public_id;
+            profilePhotoId = upload.public_id;
         }
         const user = await register(data);
         res.cookie("auth", user.cookie, {
@@ -23,6 +25,7 @@ router.post('/register', async (req, res) => {
         res.status(201).json(user);
     } catch (error) {
         console.log(error);
+        await cloudinary.v2.uploader.destroy(profilePhotoId)
         res.status(400).json({ error: error.message });
     }
 })
