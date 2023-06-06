86d0242176dd90a055c2ddba41de53bae7e44e49
Erik Todorov <eriktdrv@gmail.com>
2023-06-06 05:40:37 +0300

    Adding cors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,17 @@ const initDatabase = require('./config/database');
 const routes = require('./routes');
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'Origin', 'X-Requested-With', 'Accept', 'Cookie'],
+    credentials: true,
+    allowedHeaders: ['Content-Type, X-Authorization'],
+    optionsSuccessStatus: 200,
+}))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(routes);
 
 const PORT = 8080 || process.env.PORT;
